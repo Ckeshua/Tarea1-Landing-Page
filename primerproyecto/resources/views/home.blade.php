ff97aa45013f9799308a7efe9a0c620fce819b42
JulioFe <j.ferreiramorales@uandresbello.edu>
2021-07-05 00:03:37 -0400

    Detalles

--- primerproyecto/resources/views/home.blade.php
+++ primerproyecto/resources/views/home.blade.php
@@ -7,7 +7,6 @@
 <div class="col text-center">
 
     <a class="btn btn-outline-primary" style="margin-top: 120px;" href="/home/escaner"> Subir </a>
-    <a class="btn btn-outline-primary" style="margin-top: 120px;" href="{{ route('ListarArchivos') }}"> Subir </a>
 
 </div>
 @endsection
