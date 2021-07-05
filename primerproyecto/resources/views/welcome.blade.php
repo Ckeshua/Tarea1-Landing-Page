ebb4494927cb780a29c5e369fabfc1d0c1d15f24
Gaos700 <g.orellanasnchez@uandresbello.edu>
2021-07-04 23:32:33 -0400

    Home page nuevo y arreglo de Login

--- primerproyecto/resources/views/welcome.blade.php
+++ primerproyecto/resources/views/welcome.blade.php
@@ -16,6 +16,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
   <img src="{{asset('/partelogo.png')}}" class="img-fluid" alt="..." style="margin-left:18%">
+<<<<<<< Updated upstream
   <img src="{{asset('/logomenu1.png')}}" class="img-fluid" alt="..." >
 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -48,6 +49,69 @@
       @endguest
     </ul>
   </div>
+=======
+  <img src="{{asset('/logomenu1.png')}}" class="img-fluid" alt="...">
+  
+  @auth
+
+  <ul class="nav justify-content-end" style="margin-left 30%">
+
+    <li class="nav-item">
+      <a class="nav-link" href="/home">Archivos</a>
+
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page">Sesión iniciada</a>
+    </li>
+    <li class="nav-item">
+
+      <form action="{{route('CerrarS')}}" method="POST">
+        @csrf
+        <button type="submit" class="btn btn-primary">Cerrar sesion</button>
+      </form>
+    </li>
+
+    @endauth
+    @guest
+    <li class="nav-item" style="margin-left:40%">
+      <div class="btn-group">
+        <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+          Login
+        </button>
+        <ul class="dropdown-menu"style = "margin-left: -70px">
+          <div class="container h-100" style="width: 230px;px"style = "margin-left: -30px">
+            <div class="row justify-content-center h-100 align-items-center">
+
+              <form style="border:1px solid black;padding:1rem;border-radius: 0.5rem;" action="{{route('intentos')}}" method="post">
+
+                @csrf
+                <?php
+                if (isset($error)) {
+                  echo '<div class="alert alert-success">Usuario bloqueado durante 1 minuto</div>';;
+                }
+                ?>
+                <div class="form-group">
+                  <label for="Usuario">Nombre de Usuario</label>
+                  <input type="email" name="email" placeholder="email" value="{{old('email')}}">
+                </div>
+                <div class="form-group">
+                  <label for="Clave">Clave</label>
+                  <input type="password" name="password" placeholder="••••••••••" value="{{old('password')}}">
+                </div>
+                <div class="form-group">
+                  <button class="btn btn-primary">Ingresar</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </ul>
+      </div>
+
+    </li>
+    @endguest
+  </ul>
+>>>>>>> Stashed changes
 </nav>
 
 
